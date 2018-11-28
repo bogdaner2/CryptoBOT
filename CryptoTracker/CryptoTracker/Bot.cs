02596c6b93a7a30f88f9063623b6e85d002bae46
klesogor <lesogor.kirill@gmail.com>
2018-11-28 10:27:19 +0200

    Some changes

---
+++ CryptoTracker/CryptoTracker/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace CryptoTracker
+{
+    class Bot
+    {
+        private readonly TelegramBotClient botClient;
+
+        public Bot()
+        {
+            botClient = new TelegramBotClient("656960876:AAH_XWa0UR-dZiW2X4RMqQDDqd-bERWezWw");
+        }
+
+        private async void OnMessageRecived(object sender, MessageEventArgs messageEventArgs)
+        {
+        }
+    }
+}
